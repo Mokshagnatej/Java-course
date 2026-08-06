6f17739eec0f6269da54b4e2593dff7313aa83db
K.mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-08-06 11:42:09 +0530

    commit

--- src/arthmetic/OrderDemo.java
+++ src/arthmetic/OrderDemo.java
@@ -1,10 +1,10 @@
 package arthmetic;
 
-public class order {
+public class OrderDemo {
     public static void main (String[] args ){
         // Order of operations (PEMDAS/BODMAS)
      double result = 10 + 3 * 2 / (8 - 3);
 
-     System.out.print(result);
+     System.out.println("The result is: " + result);
     }
 }
--- src/arthmetic/augmentedassigment.java
+++ src/arthmetic/augmentedassigment.java
@@ -3,17 +3,9 @@
 public class augmentedassigment {
     public static void main (String[] args){
         // augmented assignment operators += -= *= /= %=
+     byte b = 127;
+     int a = b;
+     System.out.println(a);
 
-        int x = 18;
-        int y = 3;
-
-
-         x += y;
-         x -= y;
-         x *= y;
-            x /= y;
-            x %= y;
-
-         System.out.print(x);
     }
 }
