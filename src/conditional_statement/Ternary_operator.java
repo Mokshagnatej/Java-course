cdc02a7f2914b97ae556893dbdd5d71790a0a530
K.mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-08-11 21:51:40 +0530

    commit

---
+++ src/conditional_statement/Ternary_operator.java
@@ -0,0 +1,12 @@
+package conditional_statement;
+
+public class Ternary_operator{
+    public static void main(String[]args) {
+        int h = 3;
+        //int result = 0;
+String result = "not prime";
+        result = h%2==0 ? "prime" : "not prime";
+
+        System.out.println(result);
+    }
+}
