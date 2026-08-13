ff4a9c0e60373edbae6fa87cf655091bee8f9397
K.mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-08-13 21:38:40 +0530

    Refactor: Add default case to switch statement
    A default case is added to the switch statement to handle invalid day inputs. The test variable is also updated.

---
+++ src/while_loop.java
@@ -0,0 +1,15 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Honey"+" "+ i);
+            int j = 1;
+            while(j<=1){
+                System.out.println("Reddy"+" "+j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
