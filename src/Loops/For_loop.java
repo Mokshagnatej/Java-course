fa5779cd3ea17aac6fac98ba002b09e7a1d3fd9a
K.mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-08-14 06:28:08 +0530

    Refactor: Update for loop implementation in For_loop.java

--- src/Loops/For_loop.java
+++ src/Loops/For_loop.java
@@ -4,9 +4,12 @@ public class For_loop {
     public static void main(String[]args){
         for(int i=1;i<=5;i++){
             System.out.println("Day"+" "+i);
-       // for(int j=1;j<=9;j++){
-         //  System.out.println(" "+(j+8)+"-"+(j+9));
-          // }
+        for(int j =1;j<=1;j++){
+            System.out.println(j+" "+ "monday");
         }
+//        for(int j=1;j<=9;j++){
+//           System.out.println(" "+(j+8)+"-"+(j+9));
+//           }
+       }
     }
 }
