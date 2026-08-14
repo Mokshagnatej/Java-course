eba9b943f871528ec585bf5af6ce112aeca18d6b
K.mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-08-14 06:12:00 +0530

    Add: Create For_loop.java with basic for loop implementation

---
+++ src/Loops/For_loop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class For_loop {
+    public static void main(String[]args){
+        for(int i=1;i<=5;i++){
+            System.out.println("Day"+" "+i);
+       // for(int j=1;j<=9;j++){
+         //  System.out.println(" "+(j+8)+"-"+(j+9));
+          // }
+        }
+    }
+}
