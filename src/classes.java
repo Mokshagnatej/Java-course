2d97bd5962b5acb5eb67476e6e5ce8d055338145
Mokshag <action@local>
2026-08-27 22:33:02 +0530

    refactor: add calculator class with addition method and main execution

--- src/classes.java
+++ src/classes.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+class calculator{
+
+    public double add(double n1, double n2){
+       double r = n1 + n2;
+       return r;
+    }
+}
+
 
 public class classes {
+    public static void main(String[] args){
+        double num1 = 782.3;
+        double num2 = 324.2;
+    calculator cal = new calculator();
+    double result = cal.add(num1,num2);
+//        double sum = num1+num2;
+      System.out.println(result);
+    }
 }
