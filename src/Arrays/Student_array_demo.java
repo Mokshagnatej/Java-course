c72b4fb11ea93aab253d2ea50cb15cfed6069b98
Mokshag <action@local>
2026-09-03 06:44:48 +0530

    feat: add Student_array_demo class to demonstrate usage of student objects in an array

---
+++ src/Arrays/Student_array_demo.java
@@ -0,0 +1,34 @@
+package Arrays;
+class Student{
+    int rollno;
+    String name;
+    int marks;
+}
+public class Student_array_demo {
+    public static void main(String[]args){
+
+        Student s1 = new Student();
+        s1.rollno =1;
+        s1.name = "honey";
+        s1.marks = 90;
+
+        Student s2 = new Student();
+        s2.rollno =2;
+        s2.name = "Honvith";
+        s2.marks = 85;
+
+
+        Student s3 = new Student();
+        s3.rollno =3;
+        s3.name = "gundu";
+        s3.marks = 66;
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for(int i =0;i<students.length;i++){
+            System.out.println(students[i].rollno +" "+ students[i].name +" "+  students[i].marks);
+        }
+    }
+}
