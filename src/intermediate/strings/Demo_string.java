b8e2d64730a57fe3af649dbd928332c46ffd82bf
Mokshag <action@local>
2026-09-05 16:52:04 +0530

    feat: add Demo_string class to demonstrate string concatenation

---
+++ src/intermediate/strings/Demo_string.java
@@ -0,0 +1,8 @@
+package intermediate.strings;
+
+public class Demo_string {
+    public static void main(String[]args){
+        String name = "Honey";
+        System.out.println( name.concat("Reddy"));
+    }
+}
