30df6d116a273ce01dfe8b0a7a447097fd87cfe8
Mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-09-05 16:54:30 +0530

    feat: add uppercase demonstration for string concatenation in Demo_string class
    :wq

--- src/intermediate/strings/Demo_string.java
+++ src/intermediate/strings/Demo_string.java
@@ -4,5 +4,6 @@ public class Demo_string {
     public static void main(String[]args){
         String name = "Honey";
         System.out.println( name.concat("Reddy"));
+        System.out.println( name.concat("Reddy").toUpperCase());
     }
 }
